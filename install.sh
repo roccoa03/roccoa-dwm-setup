1da8787e6d6b9360d099c4d0330039341e6ca111
roccoa03 <roky.impiombato@gmail.com>
2024-03-31 15:30:38 +0200

    working on installation script

--- install.sh
+++ install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash 
 #The following installation script is desined to work specifically under Debian linux. 
 
-#Installing git to get code from github
-sudo apt install -y git 
-
 ## Creating folder structure 
-mkdir -p $HOME/.local/src/suckless 
+mkdir -p $HOME/.local/src/suckless  
 mkdir -p $HOME/.local/script
 mkdir -p $HOME/.config
 
 cd roccoa-dwm-setup/
-mv dwm-6.4/ dwmblocks/ dmwnu-5.2/ st/ slock-1.5/ $HOME/.local/src/suckless/
-mv .config/* $HOME/.config
+mv -r dwm-6.4/ dwmblocks/ dmwnu-5.2/ st/ slock-1.5/ $HOME/.local/src/suckless/
+mv -r .config/* $HOME/.config
 mv .xinitrc $HOME/ 
-mv .dwm/ $HOME/ 
+mv -r .dwm/ $HOME/  
+mv -r script/* $HOME/.local/script/
+
 
 #Installing dependencies to compile dwm
-sudo apt install -y make gcc libx11-dev libxft-dev libxinerama-dev xorg
+sudo apt install -y make gcc libx11-dev libxft-dev libxinerama-dev xorg 
+
+
+
+
+
